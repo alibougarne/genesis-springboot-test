3d810027457ff82784347a6daaa4cb8a5a5ac43d
alibougarne <bougarne.ali@gmail.com>
2021-09-28 15:49:12 +0200

    init genisis project

---
+++ src/test/java/com/genisis/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genisis.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
